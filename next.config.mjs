1a0174b7dedbd2ddd92f55842c01f4c9291451f3
Phillip ED Maier <pmaier983@gmail.com>
2023-03-20 22:57:34 -0400

    Configure vanilla extract next plugin properly

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,21 @@
-// @ts-check
+import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin"
 
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
+!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"))
+
+const withVanillaExtract = createVanillaExtractPlugin()
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
-  /**
-   * If you have the "experimental: { appDir: true }" setting enabled, then you
-   * must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
+  swcMinify: true,
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
-};
-export default config;
+}
+
+module.exports = withVanillaExtract(config)
