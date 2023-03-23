ecac7c7d68bdf13ebd6edf85b903aa9fffb26f83
Phillip ED Maier <pmaier983@gmail.com>
2023-03-22 21:36:58 -0400

    Setup Auth Page

---
+++ src/components/AuthPage.tsx
@@ -0,0 +1,26 @@
+import { signIn, signOut, useSession } from "next-auth/react"
+
+import { api } from "~/utils/api"
+
+export const AuthPage = () => {
+  const { data: sessionData } = useSession()
+
+  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
+    undefined, // no input
+    { enabled: sessionData?.user !== undefined },
+  )
+
+  return (
+    <div>
+      <p>
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+        {secretMessage && <span> - {secretMessage}</span>}
+      </p>
+      <button
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  )
+}
