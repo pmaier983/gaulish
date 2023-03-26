e1c6b2b7864392a20e140cbad8a9d1e3ec7a0205
Phillip ED Maier <pmaier983@gmail.com>
2023-03-25 20:50:51 -0400

    Rip Out css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,6 @@ import { SessionProvider } from "next-auth/react"
 
 import { api } from "~/utils/api"
 
-import "~/styles/globals.css"
-
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
