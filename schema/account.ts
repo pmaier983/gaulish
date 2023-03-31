380f179a66384172ba2e6653023149c08b93d66f
Phillip ED Maier <pmaier983@gmail.com>
2023-03-30 22:31:40 -0400

    Final transfer to drizzle-orm

--- schema/account.ts
+++ schema/account.ts
@@ -1,80 +1,92 @@
 import {
   datetime,
+  index,
   int,
   mysqlTable,
-  serial,
   text,
+  uniqueIndex,
   varchar,
 } from "drizzle-orm/mysql-core"
 
 // TODO: rip out all of these table and add non prisma dependent ones in
 
-export const account = mysqlTable("Account", {
-  id: serial("id").primaryKey(), // TODO: @default(cuid())
-  userId: varchar("userId", { length: 191 }),
-  type: varchar("type", { length: 191 }),
-  provider: varchar("provider", { length: 191 }),
-  providerAccountId: varchar("providerAccountId", { length: 191 }),
-  refresh_token: text("refresh_token"),
-  access_token: text("access_token"),
-  expires_at: int("expires_at"),
-  token_type: varchar("token_type", { length: 191 }),
-  scope: varchar("scope", { length: 191 }),
-  id_token: text("id_token"),
-  session_state: varchar("session_state", { length: 191 }),
-})
+export const account = mysqlTable(
+  "Account",
+  {
+    id: varchar("id", { length: 191 }).primaryKey().notNull(),
+    userId: varchar("userId", { length: 191 }).notNull(),
+    type: varchar("type", { length: 191 }).notNull(),
+    provider: varchar("provider", { length: 191 }).notNull(),
+    providerAccountId: varchar("providerAccountId", { length: 191 }).notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: int("expires_at"),
+    token_type: varchar("token_type", { length: 191 }),
+    scope: varchar("scope", { length: 191 }),
+    id_token: text("id_token"),
+    session_state: varchar("session_state", { length: 191 }),
+  },
+  (account) => ({
+    Account_provider_providerAccountId_key: uniqueIndex(
+      "Account_provider_providerAccountId_key",
+    ).on(account.provider, account.providerAccountId),
+    Account_userId_idx: index("Account_userId_idx").on(account.userId),
+  }),
+)
 
 // TODO: separate out sql table schema's?
 export const example = mysqlTable("Example", {
-  id: serial("id").primaryKey(), // TODO: @default(cuid())
-  createdAt: datetime("createdAt"), // TODO: @default(now())
-  updatedAt: datetime("updatedAt"), // TODO: @updatedAt
+  id: varchar("id", { length: 191 })
+    .primaryKey()
+    .notNull()
+    .default(new Date().getTime().toString()),
+  createdAt: datetime("createdAt").notNull(),
+  updatedAt: datetime("updatedAt").notNull(),
 })
 
-// model Account {
-// id                 String  @id @default(cuid())
-// userId             String
-// type               String
-// provider           String
-// providerAccountId  String
-// refresh_token      String?  @db.Text
-// access_token       String?  @db.Text
-// expires_at         Int?
-// token_type         String?
-// scope              String?
-// id_token           String?  @db.Text
-// session_state      String?
+export const session = mysqlTable(
+  "Session",
+  {
+    id: varchar("id", { length: 191 }).primaryKey().notNull(),
+    sessionToken: varchar("sessionToken", { length: 191 }).notNull(),
+    userId: varchar("userId", { length: 191 }).notNull(),
+    expires: datetime("expires").notNull(),
+  },
+  (session) => ({
+    Session_sessionToken_key: uniqueIndex("Session_sessionToken_key").on(
+      session.sessionToken,
+    ),
+    Session_userId_idx: index("Session_userId_idx").on(session.userId),
+  }),
+)
 
-// user User @relation(fields: [userId], references: [id], onDelete: Cascade)
+export const user = mysqlTable(
+  "User",
+  {
+    id: varchar("id", { length: 191 }).primaryKey().notNull(),
+    name: varchar("name", { length: 191 }),
+    email: varchar("email", { length: 191 }),
+    emailVerified: datetime("emailVerified"),
+    image: text("image"),
+  },
+  (user) => ({
+    User_email_key: uniqueIndex("User_email_key").on(user.email),
+  }),
+)
 
-// @@unique([provider, providerAccountId])
-// @@index([userId])
-// }
-
-// model Session {
-// id           String   @id @default(cuid())
-// sessionToken String   @unique
-// userId       String
-// expires      DateTime
-// user         User     @relation(fields: [userId], references: [id], onDelete: Cascade)
-
-// @@index([userId])
-// }
-
-// model User {
-// id            String    @id @default(cuid())
-// name          String?
-// email         String?   @unique
-// emailVerified DateTime?
-// image         String?   @db.Text
-// accounts      Account[]
-// sessions      Session[]
-// }
-
-// model VerificationToken {
-// identifier String
-// token      String   @unique
-// expires    DateTime
-
-// @@unique([identifier, token])
-// }
+export const verificationToken = mysqlTable(
+  "VerificationToken",
+  {
+    identifier: varchar("identifier", { length: 191 }).notNull(),
+    token: varchar("token", { length: 191 }).notNull(),
+    expires: datetime("expires").notNull(),
+  },
+  (verificationToken) => ({
+    VerificationToken_token_key: uniqueIndex("VerificationToken_token_key").on(
+      verificationToken.token,
+    ),
+    VerificationToken_identifier_token_key: uniqueIndex(
+      "VerificationToken_identifier_token_key",
+    ).on(verificationToken.identifier, verificationToken.token),
+  }),
+)
