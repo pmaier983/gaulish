ca0c06df06375a07f126564496cc1029947f6ca4
Phillip ED Maier <pmaier983@gmail.com>
2023-04-18 22:58:52 -0400

    stylint

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,8 +1,10 @@
+const camelCaseRegex = "^[a-z][\\w-]*$" // Note: also allows `_` as part of BEM naming
+
 /** @type {import("stylelint").Config} */
 const config = {
   extends: ["stylelint-config-standard"],
   rules: {
-    "selector-class-pattern": "^[a-z][\\w-]*$",
+    "selector-class-pattern": camelCaseRegex,
   },
 }
 
