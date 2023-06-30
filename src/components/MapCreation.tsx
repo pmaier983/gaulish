2d752c9b4902b660f652409de954a84d76e25ed1
Phillip ED Maier <pmaier983@gmail.com>
2023-06-30 06:42:47 -0700

    fix the graphic

--- src/components/MapCreation.tsx
+++ src/components/MapCreation.tsx
@@ -1,13 +1,11 @@
 import { Sprite, Stage } from "@pixi/react"
 import { PixieViewport } from "~/components/PixieViewport"
 
-import grass from "~/../public/grass.png"
-
 export const MapCreation = () => {
   return (
     <Stage width={100} height={100} options={{ backgroundColor: 0x1099bb }}>
       <PixieViewport width={100} height={100}>
-        <Sprite x={20} y={20} anchor={0.5} image={grass} />
+        <Sprite x={20} y={20} anchor={0.5} />
       </PixieViewport>
     </Stage>
   )
