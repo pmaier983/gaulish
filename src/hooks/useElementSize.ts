add819df882fcce5603a163c7fdd7220cfdc0c0b
Phillip ED Maier <pmaier983@gmail.com>
2023-07-07 07:37:23 -0700

    Setup element size hook

---
+++ src/hooks/useElementSize.ts
@@ -0,0 +1,42 @@
+import { useCallback, useLayoutEffect, useRef, useState } from "react"
+
+interface Size {
+  width: number
+  height: number
+}
+
+/**
+ * Returns the size of the element and a ref to attach to the element.
+ *
+ * @returns \{\{width, height\}, sizeRef\}
+ */
+export const useElementSize = <T extends HTMLElement = HTMLDivElement>() => {
+  const sizeRef = useRef<T>(null)
+
+  const [isInitialSizeSet, setInitialFlag] = useState(false)
+  const [size, setSize] = useState<Size>({
+    width: 0,
+    height: 0,
+  })
+
+  const handleSize = useCallback(() => {
+    setSize({
+      width: sizeRef.current?.offsetWidth || 0,
+      height: sizeRef.current?.offsetHeight || 0,
+    })
+  }, [])
+
+  useLayoutEffect(() => {
+    if (!isInitialSizeSet) {
+      handleSize()
+      setInitialFlag(true)
+    }
+
+    window.addEventListener("resize", handleSize)
+    return () => {
+      window.removeEventListener("resize", handleSize)
+    }
+  }, [handleSize, isInitialSizeSet])
+
+  return { size, sizeRef }
+}
