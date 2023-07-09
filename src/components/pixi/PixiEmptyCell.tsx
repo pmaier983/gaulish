ac729d1bff6b6170a870303aa98f1e50adb1eeca
Phillip ED Maier <pmaier983@gmail.com>
2023-07-08 19:18:08 -0700

    fix dumb rectangle issues with empty cell

--- src/components/pixi/PixiEmptyCell.tsx
+++ src/components/pixi/PixiEmptyCell.tsx
@@ -18,29 +18,17 @@ export const PixiEmptyCell = ({
 }: PixiEmptyCellProps) => {
   const app = useApp()
 
-  const cellWidth = (app.renderer.options.width ?? 0) * percentSize
-  const cellHeight = (app.renderer.options.height ?? 0) * percentSize
+  const cellSize = (app.renderer.options.width ?? 0) * percentSize
 
   const draw = useCallback(
     (g: PIXI.Graphics) => {
       g.clear()
       g.beginFill(fill)
-      g.lineStyle(
-        (app.renderer.options.width ?? 0) * percentSize * (1 / 10),
-        0x000000,
-      )
-      g.drawRect(x, y, cellWidth, cellHeight)
+      g.lineStyle(cellSize * (1 / 10), 0x000000)
+      g.drawRect(x, y, cellSize, cellSize)
       g.endFill()
     },
-    [
-      app.renderer.options.width,
-      cellHeight,
-      cellWidth,
-      fill,
-      percentSize,
-      x,
-      y,
-    ],
+    [cellSize, fill, x, y],
   )
   return <Graphics draw={draw} {...rest} />
 }
