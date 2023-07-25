04faf7f06ccbf0933e20dd71d62ccdffc8f92784
Phillip ED Maier <pmaier983@gmail.com>
2023-07-24 20:39:26 -0700

    Style Leaderboard

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -35,16 +35,27 @@ export const Leaderboard = () => {
 
 // TODO: should I be using ID's and not emails here?
 const LeaderboardList = ({ emails }: { emails: string[] }) => {
-  const { data } = api.general.getLeaderboard.useQuery(emails, {
+  const { data } = api.general.getLeaderboard.useQuery(emails.slice(0, 10), {
     enabled: emails.length > 0,
   })
   return (
-    <div>
-      {data?.map((user) => (
-        <div key={user.username}>
-          {user.username}-{user.gold}
-        </div>
+    <ol
+      className="flex h-full list-decimal flex-col items-center justify-start  overflow-y-auto"
+      type="1"
+    >
+      {data?.map((user, i) => (
+        <li
+          key={user.username}
+          className="flex w-full justify-between pl-5 pr-5"
+        >
+          <div>
+            {i + 1}
+            {")"}
+          </div>
+          <h3>{user.username}</h3>
+          <div>{user.gold}</div>
+        </li>
       ))}
-    </div>
+    </ol>
   )
 }
