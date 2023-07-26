ac1a821b06f542df5f6a665bfa0330d73eeb1c28
Phillip ED Maier <pmaier983@gmail.com>
2023-07-26 07:59:13 -0700

    Fix, Cleanup & add ship based api paths

--- src/server/api/routers/general.ts
+++ src/server/api/routers/general.ts
@@ -1,12 +1,13 @@
 import { eq, inArray } from "drizzle-orm"
-import { type Npc, type Path, npc, path, tile, users } from "schema"
+import { type Npc, type Path, npc, path, tile, users, ship } from "schema"
+import { z } from "zod"
+import { type PlanetScaleDatabase } from "drizzle-orm/planetscale-serverless"
+
 import {
   SHIP_ID_TO_SHIP_TYPES,
   TILE_TYPES,
   TILE_TYPE_TO_TYPE_ID,
 } from "~/components/constants"
-import { z } from "zod"
-
 import {
   createTRPCRouter,
   protectedProcedure,
@@ -24,8 +25,23 @@ import { getPathFromString } from "~/utils/utils"
 // - getUserShipPaths
 //   - update this list constantly with websockets.
 
+interface GetUserFromEmail {
+  email?: string | null
+  db: PlanetScaleDatabase<Record<string, never>>
+}
+
+// TODO: add user id to the session!!!!
+const getUserFromEmail = async ({ email, db }: GetUserFromEmail) => {
+  if (!email) throw new Error("No user email found in session")
+  // TODO: how to get userId in the users session?
+  const user = (
+    await db.select().from(users).where(eq(users.email, email)).limit(1)
+  ).at(0)
+  if (!user) throw new Error("No user found with that email")
+  return user
+}
+
 export const generalRouter = createTRPCRouter({
-  // TODO: implement map rendering on the client
   getAllTiles: protectedProcedure.query(({ ctx }) => {
     return ctx.db.select().from(tile)
   }),
@@ -41,15 +57,44 @@ export const generalRouter = createTRPCRouter({
     })
   }),
   /**
+   * Adds a ship to a users profile
+   */
+  addShip: protectedProcedure
+    .input(
+      z.object({
+        ship_type_id: z.number(),
+        city_id: z.number(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const user = await getUserFromEmail({
+        email: ctx.session.user.email,
+        db: ctx.db,
+      })
+
+      return ctx.db.insert(ship).values({
+        shipTypeId: input.ship_type_id,
+        userId: user.id,
+        cityId: input.city_id,
+        cargo: {},
+      })
+    }),
+  /**
+   * Fetch a list of the users Ships
+   */
+  getUsersShips: protectedProcedure.query(async ({ ctx }) => {
+    const user = await getUserFromEmail({
+      email: ctx.session.user.email,
+      db: ctx.db,
+    })
+    return ctx.db.select().from(ship).where(eq(ship.userId, user.id))
+  }),
+  /**
    * Given a list of emails, return a sorted leaderboard of {usernames, gold}
-   *
-   * @param {Array<string>} input - An array of email addresses of users to retrieve from the leaderboard.
-   * @returns {Promise<Array<{ username: string, gold: number }>>} - A Promise that resolves to an array of objects containing the username and gold points of the users in descending order based on their gold points.
    */
   getLeaderboard: protectedProcedure
     .input(z.string().array())
     .query(async ({ ctx, input }) => {
-      throw Error("shucks")
       const leaderboardUsers = await ctx.db
         .select()
         .from(users)
@@ -61,6 +106,9 @@ export const generalRouter = createTRPCRouter({
           gold: user.gold,
         }))
     }),
+  /**
+   * Fetch a list of NPCs with their ships & paths attached
+   */
   getNpcs: protectedProcedure.query(({ ctx }) => {
     return ctx.db
       .select()
