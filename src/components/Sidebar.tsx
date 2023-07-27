1bfeb8588b29ccf2dea6615915ed01b2bd65cbd4
Phillip ED Maier <pmaier983@gmail.com>
2023-07-26 20:29:18 -0700

    basic use of get ships

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,18 @@
+import { api } from "~/utils/api"
+
+// TODO: handle cities next!
 export const Sidebar = () => {
-  return <div className="flex-1">Sidebar heyo</div>
+  const { data, isSuccess } = api.general.getUsersShips.useQuery()
+  const { mutate } = api.general.addShip.useMutation({})
+  return (
+    <div className="flex-1">
+      {data?.length === 0 && isSuccess && (
+        <button
+          onClick={() => {
+            // mutate({city_id: 1, })
+          }}
+        ></button>
+      )}
+    </div>
+  )
 }
