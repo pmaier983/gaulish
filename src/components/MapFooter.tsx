25b8c009d4a5195fedc7f29586d7f76df9bf1a1f
Phillip ED Maier <pmaier983@gmail.com>
2023-08-15 22:32:52 -0400

    Style sail and Cancel Buttons appropriately

--- src/components/MapFooter.tsx
+++ src/components/MapFooter.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react"
+import React, { useCallback } from "react"
 import { useGamestateStore } from "~/state/gamestateStore"
 
 import styles from "./mapFooter.module.css"
@@ -8,9 +8,16 @@ interface MapFooterProps {
 }
 
 export const MapFooter = ({ className = "" }: MapFooterProps) => {
-  const { selectedShip, toggleShipSelection } = useGamestateStore(
+  const {
+    selectedShip,
+    cityObject,
+    selectedShipPathArray,
+    toggleShipSelection,
+  } = useGamestateStore(
     useCallback(
       (state) => ({
+        cityObject: state.cityObject,
+        selectedShipPathArray: state.selectedShipPathArray,
         selectedShip: state.selectedShip,
         toggleShipSelection: state.toggleShipSelection,
       }),
@@ -20,20 +27,27 @@ export const MapFooter = ({ className = "" }: MapFooterProps) => {
 
   if (!selectedShip) return null
 
+  const isShipSailingToKnownCity =
+    selectedShipPathArray.length > 1 &&
+    cityObject.hasOwnProperty(selectedShipPathArray.at(-1) ?? "")
+
   return (
     <div className={`${className} ${styles.footerContainer ?? ""}`}>
       <button
         className={styles.cancelButton}
-        onClick={void toggleShipSelection}
+        onClick={() => toggleShipSelection()}
       >
         Cancel
       </button>
       <div className={styles.gap} />
       <button
-        className={styles.sailButton}
+        className={`${styles.sailButton ?? ""} ${
+          isShipSailingToKnownCity ? styles.sailButtonPure ?? "" : ""
+        }`}
         onClick={() => {
           console.log(selectedShip)
         }}
+        disabled={selectedShipPathArray.length === 1}
       >
         Sail
       </button>
