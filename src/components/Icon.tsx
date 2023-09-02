17d05582444fb5d87fdfdb5f44a37fff52ecf5ef
Phillip ED Maier <pmaier983@gmail.com>
2023-09-02 14:32:55 -0400

    add edit-3 icon

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -6,20 +6,24 @@ import { type SVGAttributes } from "react"
 export const ICONS = {
   x: "x",
   arrowRight: "arrow-right",
+  "edit-3": "edit-3",
 } as const
 
 export type ICON = keyof typeof ICONS
 
 interface IconProps extends SVGAttributes<SVGSVGElement> {
   id: ICON
+  size?: SVGAttributes<SVGSVGElement>["width"]
 }
 
 // Inspired by this article: https://benadam.me/thoughts/react-svg-sprites/
 // TODO: decide if this is the best svg method, or another way would be better?
-export const Icon = (props: IconProps) => {
-  return (
-    <svg height={24} width={24} {...props}>
-      <use href={`/icons.svg#${ICONS[props.id]}`} />
-    </svg>
-  )
-}
+export const Icon = (props: IconProps) => (
+  <svg
+    height={props.size ?? props.width ?? 24}
+    width={props.size ?? props.height ?? 24}
+    {...props}
+  >
+    <use href={`/icons.svg#${ICONS[props.id]}`} />
+  </svg>
+)
