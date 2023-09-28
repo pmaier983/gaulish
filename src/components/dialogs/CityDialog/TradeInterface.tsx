d6bf9b9bc4bd988da64899765ebdfbc950540b8a
Phillip ED Maier <pmaier983@gmail.com>
2023-09-27 22:02:14 -0400

    Initial trade slider interface

--- src/components/dialogs/CityDialog/TradeInterface.tsx
+++ src/components/dialogs/CityDialog/TradeInterface.tsx
@@ -1,12 +1,15 @@
 import { type ComponentPropsWithRef } from "react"
 import { type City } from "schema"
 import { CityTradeCard } from "~/components/CityTradeCard"
+import { FormatNumber } from "~/components/FormatNumber"
+import { ImageIcon } from "~/components/ImageIcon"
 import { CargoCount, ImageIconCount } from "~/components/ImageIconCount"
+import { PriceSlider } from "~/components/PriceSlider"
 import { ShipHeader } from "~/components/ShipHeader"
 import { ShipTradeCard } from "~/components/ShipTradeCard"
 import { ExitButton } from "~/components/buttons/ExitButton"
 import { type ShipComposite } from "~/state/gamestateStore"
-import { getCargoSum } from "~/utils/utils"
+import { getCargoSum, getPrice } from "~/utils/utils"
 
 export interface TradeInterfaceProps extends ComponentPropsWithRef<"div"> {
   tradeShip?: ShipComposite
@@ -50,7 +53,43 @@ export const TradeInterface = ({
         toggleSelectedCityId={toggleSelectedCityId}
         toggleSelectedTradeShipId={toggleSelectedTradeShipId}
       />
-      <div className="flex flex-1 flex-row">TODO</div>
+      {selectedCity.cityCargo.map((cargo) => {
+        const cargoPrice = getPrice({ ...cargo, seed: selectedCity.id })
+
+        return (
+          <div
+            key={cargo.type}
+            className="grid grid-cols-[1fr_4rem_1fr] border-b-2 border-dashed border-black pb-2 pt-2"
+          >
+            <div className="flex flex-1 flex-row pl-2 pr-2">
+              <PriceSlider
+                price={cargoPrice}
+                type="SELL"
+                ship={tradeShip}
+                cargoType={cargo.type}
+                onSubmit={(val) => {
+                  console.log("wow!", val)
+                }}
+              />
+            </div>
+            <div className="flex min-w-[4rem] flex-row content-center items-center gap-2">
+              <ImageIcon id={cargo.type} />
+              <FormatNumber number={cargoPrice} isGold />
+            </div>
+            <div className="flex flex-1 flex-row ">
+              <PriceSlider
+                price={cargoPrice}
+                type="BUY"
+                ship={tradeShip}
+                cargoType={cargo.type}
+                onSubmit={(val) => {
+                  console.log("wow!", val)
+                }}
+              />
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
