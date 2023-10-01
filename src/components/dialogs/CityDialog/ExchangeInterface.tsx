da75d79311ffa21c0f1e8924c22f864dc1a20c0c
Phillip ED Maier <pmaier983@gmail.com>
2023-10-01 08:10:21 -0400

    Begin some Exchange interface work

--- src/components/dialogs/CityDialog/ExchangeInterface.tsx
+++ src/components/dialogs/CityDialog/ExchangeInterface.tsx
@@ -1,16 +1,41 @@
 import { type ComponentPropsWithRef } from "react"
+import { type City } from "schema"
 import { type ShipComposite } from "~/state/gamestateStore"
 
 export interface ExchangeInterfaceProps extends ComponentPropsWithRef<"div"> {
-  selectedExchangeShips?: ShipComposite[]
+  selectedExchangeShipLeft?: ShipComposite
+  selectedExchangeShipRight?: ShipComposite
+  selectedCity?: City
 }
 
-export const ExchangeInterface = ({ className }: ExchangeInterfaceProps) => {
+export const ExchangeInterface = ({
+  selectedExchangeShipLeft,
+  selectedExchangeShipRight,
+  className,
+}: ExchangeInterfaceProps) => {
+  if (!selectedExchangeShipLeft || !selectedExchangeShipRight) {
+    return <div></div>
+  }
+
   return (
     <div
       className={`flex max-w-full flex-1 gap-2 overflow-x-auto p-2 ${className}`}
     >
-      Exchange
+      <ExchangeInterfaceHeader
+        selectedExchangeShipLeft={selectedExchangeShipLeft}
+        selectedExchangeShipRight={selectedExchangeShipRight}
+      />
     </div>
   )
+}
+
+interface ExchangeInterfaceHeaderProps extends ComponentPropsWithRef<"div"> {
+  selectedExchangeShipLeft?: ShipComposite
+  selectedExchangeShipRight?: ShipComposite
+}
+
+const ExchangeInterfaceHeader = ({} // selectedExchangeShipLeft,
+// selectedExchangeShipRight,
+: ExchangeInterfaceHeaderProps) => {
+  return <div className="grid flex-1 grid-cols-2"></div>
 }
