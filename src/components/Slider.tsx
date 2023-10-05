1c1ad967eacf1ea7bbeb16942d056eaf73d2272a
Phillip ED Maier <pmaier983@gmail.com>
2023-10-04 22:09:59 -0400

    Update Slider css

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -7,15 +7,15 @@ export interface SliderProps extends RadixSlider.SliderProps {
 export const Slider = ({ label, className = "", ...rest }: SliderProps) => {
   return (
     <RadixSlider.Root
-      className={`relative flex flex-1 touch-none select-none items-center ${className}`}
+      className={`relative flex flex-1 cursor-pointer touch-none select-none items-center ${className}`}
       {...rest}
     >
       <RadixSlider.Track className="relative h-2 flex-1 rounded bg-slate-400 data-[disabled]:opacity-50">
         <RadixSlider.Range className="absolute h-full rounded bg-amber-400" />
       </RadixSlider.Track>
       <RadixSlider.Thumb
-        className="block h-4 w-4 rounded-full bg-amber-900 data-[disabled]:opacity-0"
         aria-label={label}
+        className="m-3 block h-5 w-5 rounded-full bg-amber-900 hover:bg-amber-600 active:bg-amber-400 data-[disabled]:opacity-0"
       />
     </RadixSlider.Root>
   )
