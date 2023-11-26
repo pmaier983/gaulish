399bf5aba49e6b03e111171d4924fc65f7ff48ae
Phillip ED Maier <pmaier983@gmail.com>
2023-11-25 22:31:27 -0500

    Setup MapCreation Map

--- src/components/map/MapCreation/MapToppings.tsx
+++ src/components/map/MapCreation/MapToppings.tsx
@@ -1,5 +1,18 @@
+import dynamic from "next/dynamic"
 import type { Tile } from "schema"
 import type { SetMapCreationMode } from "~/components/map/MapCreation/constants"
+import { MapGroupedPixiTileBase } from "~/components/map/MapGroupedBaseTiles"
+import { useElementSize } from "~/hooks/useElementSize"
+
+const MapWrapper = dynamic(
+  () =>
+    import("~/components/map/MapWrapper").then(
+      (allExports) => allExports.MapWrapper_DO_NOT_USE_DIRECTLY,
+    ),
+  {
+    ssr: false,
+  },
+)
 
 interface MapToppingsProps {
   className?: string
@@ -11,6 +24,16 @@ interface MapToppingsProps {
   setMapSize: (newSize: number) => void
 }
 
-export const MapToppings = ({ className }: MapToppingsProps) => {
-  return <div className={`${className}`}></div>
+export const MapToppings = ({ className, mapArray }: MapToppingsProps) => {
+  const { size, sizeRef } = useElementSize()
+  return (
+    <div className={`${className} flex flex-1 flex-row gap-8 p-8`}>
+      <div className="flex flex-1" ref={sizeRef}>
+        <MapWrapper mapHeight={size.height} mapWidth={size.width}>
+          <MapGroupedPixiTileBase mapArray={mapArray} />
+        </MapWrapper>
+      </div>
+      <div className="flex-1">Hello</div>
+    </div>
+  )
 }
