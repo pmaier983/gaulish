86a4dc6023fd210ed8fc55eddbbf0f7a6b7f7d94
Phillip ED Maier <pmaier983@gmail.com>
2024-02-04 12:44:32 -0500

    Add comment detailing the new schema change

--- schema/schema.ts
+++ schema/schema.ts
@@ -20,6 +20,8 @@ import {
   type TileType,
 } from "../src/components/constants"
 
+// This is essentially a db prefix, allowing us to have multiple projects on the same db easily
+// This is 100% not a recommended pattern for a real product...
 const mysqlTable = mysqlTableCreator((name) => `gaulish_${name}`)
 
 /* ******************** START - DEFAULT STUFF FROM NEXTAUTH ******************** */
